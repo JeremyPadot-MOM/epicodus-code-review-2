1e0e6e8a38c668c80e58fda5f96840b2ac2cb0e3
Jeremy Kale Padot <jeremy@padot.us>
2020-01-17 13:17:33 -0800

    add first <= value to show result in js. not yet working

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-$("form#survey").submit(fuction){
+$("form#survey").submit(fuction(){
   event.preventDefault();
+  var result = calculate()
+  console.log("result is: " + result)
+  if (result <= 5){
+    $("#react").show();
+    $("#c").hide();
+    $("#js").hide();
+    $("#doom").hide();
+  }
 }
 });
